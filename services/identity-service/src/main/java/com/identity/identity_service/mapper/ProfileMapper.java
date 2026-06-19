310656dd5e8536d6d44f7974572fbba8d54b801f
Humanoid1266 <tridq6280@ut.edu.vn>
2026-06-19 13:10:47 +0700

    fix(identity): fix ProfileMapper componentModel Spring → spring

--- services/identity-service/src/main/java/com/identity/identity_service/mapper/ProfileMapper.java
+++ services/identity-service/src/main/java/com/identity/identity_service/mapper/ProfileMapper.java
@@ -4,7 +4,7 @@ import com.identity.identity_service.dto.request.CreateUserRequest;
 import com.identity.identity_service.dto.request.ProfileRequest;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "Spring")
+@Mapper(componentModel = "spring")
 public interface ProfileMapper {
     ProfileRequest toProfileRequest(CreateUserRequest createUserRequest);
 }
