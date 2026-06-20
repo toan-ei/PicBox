72d8df58d73ad5b04b06f6868f49e42fb28d760f
Humanoid1266 <tridq6280@ut.edu.vn>
2026-06-20 18:10:39 +0700

    build docker

--- docker/mysql-init/01-create-databases.sql
+++ docker/mysql-init/01-create-databases.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS order_service CHARACTER SET utf8mb4 COLLATE utf8mb
 CREATE DATABASE IF NOT EXISTS payment_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS hub_branch_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS staff_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS notification_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 GRANT ALL PRIVILEGES ON identity_service.* TO 'dev'@'%';
 GRANT ALL PRIVILEGES ON profile_service.* TO 'dev'@'%';
@@ -11,4 +12,5 @@ GRANT ALL PRIVILEGES ON order_service.* TO 'dev'@'%';
 GRANT ALL PRIVILEGES ON payment_service.* TO 'dev'@'%';
 GRANT ALL PRIVILEGES ON hub_branch_service.* TO 'dev'@'%';
 GRANT ALL PRIVILEGES ON staff_service.* TO 'dev'@'%';
+GRANT ALL PRIVILEGES ON notification_service.* TO 'dev'@'%';
 FLUSH PRIVILEGES;
